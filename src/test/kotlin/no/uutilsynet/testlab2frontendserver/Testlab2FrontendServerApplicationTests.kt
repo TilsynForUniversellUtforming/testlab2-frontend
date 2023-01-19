377b002f74af1840edb0a5403bc2460207af0429
Lars Lønne <lonnelars@gmail.com>
2023-01-19 14:40:22 +0100

    lager en server for frontend
    Lager en spring boot server for å serve frontenden. Skal etterhvert brukes for autentisering og som proxy for api-kall.

---
+++ src/test/kotlin/no/uutilsynet/testlab2frontendserver/Testlab2FrontendServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.uutilsynet.testlab2frontendserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Testlab2FrontendServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
