11108ebb123ea997abd5548e51b279f6e7293269
Lars Lønne <lonnelars@gmail.com>
2023-01-31 13:26:01 +0100

    lister opp målinger
    Utvider frontend-apiet med endepunkter for å hente alle målinger, og viser dem i web appen. Dette er fortsatt et utkast, men er skjult bak en toggle slik at vi kan merge det nå.
    
    DFK-79

---
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/Features.kt
@@ -0,0 +1,19 @@
+package no.uutilsynet.testlab2frontendserver
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/v1/features")
+class Features(val featuresProperties: FeaturesProperties) {
+  @GetMapping
+  fun list(): List<Feature> {
+    return listOf(Feature("maalinger", featuresProperties.maalinger))
+  }
+}
+
+data class Feature(val key: String, val active: Boolean)
+
+@ConfigurationProperties(prefix = "features") data class FeaturesProperties(val maalinger: Boolean)
