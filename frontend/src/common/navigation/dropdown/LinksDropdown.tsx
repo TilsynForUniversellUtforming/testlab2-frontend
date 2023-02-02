70b7608a91dc0628a0c2a8c996782a411377c578
Tore Fotland <tore.jacobsen.fotland@digdir.no>
2023-02-02 13:29:02 +0100

    [BUGFIX] Kontrast i hovedmeny

--- frontend/src/common/navigation/dropdown/LinksDropdown.tsx
+++ frontend/src/common/navigation/dropdown/LinksDropdown.tsx
@@ -1,9 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-import { Route } from '../routes';
+import { Route } from '../../routes';
 
 interface Props {
   navn: string;
@@ -12,7 +10,7 @@ interface Props {
 
 export const LinksDropdown = ({ navn, routes }: Props) => {
   return (
-    <NavDropdown title={navn}>
+    <NavDropdown title={navn} className="navbar__dropdown">
       {routes.map((route) => (
         <NavDropdown.Item key={route.navn} as={Link} to={route.path}>
           {route.navn}
