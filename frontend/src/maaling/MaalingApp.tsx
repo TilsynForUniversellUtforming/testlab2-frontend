0e55456728e7a5ea8f62f1f5f8becec44d4655cd
Lars Lønne <lonnelars@gmail.com>
2023-02-10 14:14:51 +0100

    utvider målinger med navn
    Nye målinger opprettes med et navn og en url, og målinger hentes fra databasen med navn og url.

--- frontend/src/maaling/MaalingApp.tsx
+++ frontend/src/maaling/MaalingApp.tsx
@@ -9,6 +9,7 @@ import DigdirTable from '../common/table/DigdirTable';
 
 type Maaling = {
   id: number;
+  navn: string;
   url: string;
 };
 type FetchingData = { state: 'fetching-data' };
@@ -86,6 +87,7 @@ function Maalinger({ state }: MaalingerProps) {
       accessorFn: (row) => row.id,
       enableColumnFilter: false,
     },
+    { id: 'Navn', accessorFn: (row) => row.navn, enableColumnFilter: false },
     { id: 'URL', accessorFn: (row) => row.url, enableColumnFilter: false },
   ];
 
@@ -133,10 +135,11 @@ interface NyMaalingProps {
 }
 
 function NyMaaling({ onNewMaaling }: NyMaalingProps) {
-  const [form, setForm] = useState({ url: '' });
+  const initialForm = { navn: '', url: '' };
+  const [form, setForm] = useState(initialForm);
 
   function clearForm() {
-    setForm({ url: '' });
+    setForm(initialForm);
   }
 
   function handleSubmit(event: FormEvent) {
@@ -146,27 +149,44 @@ function NyMaaling({ onNewMaaling }: NyMaalingProps) {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ url: form.url }),
+      body: JSON.stringify(form),
     })
       .then(clearForm)
       .then(onNewMaaling);
   }
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    setForm({ ...form, url: event.currentTarget.value });
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    setForm({ ...form, [name]: value });
   }
 
   return (
     <section>
       <h3>Ny måling</h3>
       <Form onSubmit={handleSubmit}>
-        <Form.Label>URL:</Form.Label>
-        <Form.Control
-          type="text"
-          value={form.url}
-          onChange={handleChange}
-        ></Form.Control>
-        <Button type="submit">Lagre</Button>
+        <Form.Group className="mb-3">
+          <Form.Label>Navn:</Form.Label>
+          <Form.Control
+            type="text"
+            name="navn"
+            value={form.navn}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>URL:</Form.Label>
+          <Form.Control
+            type="text"
+            name="url"
+            value={form.url}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Lagre
+        </Button>
       </Form>
     </section>
   );
