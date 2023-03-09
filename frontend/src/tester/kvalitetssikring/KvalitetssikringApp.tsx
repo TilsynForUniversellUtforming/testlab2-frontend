cadd0dbd4c2f7bfbf93a6b15efb34c9828972b0f
Tore Fotland <tore.jacobsen.fotland@digdir.no>
2023-03-09 14:11:49 +0100

    DFK-141 - [WIP] Lister urler fra crawling i Kvalitetssikring

--- frontend/src/tester/kvalitetssikring/KvalitetssikringApp.tsx
+++ frontend/src/tester/kvalitetssikring/KvalitetssikringApp.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Spinner } from 'react-bootstrap';
-import { useOutletContext } from 'react-router-dom';
+import { useOutletContext, useParams } from 'react-router-dom';
 
 import ErrorCard from '../../common/error/ErrorCard';
+import { CrawlUrl } from '../../maaling/types';
 import { TesterContext } from '../types';
 import KvalitetssikringList from './KvalitetssikringList';
 
 const KvalitetssikringApp = () => {
+  const { loeysingId } = useParams();
   const { error, loading, maaling }: TesterContext = useOutletContext();
-
-  const loesyingList = maaling?.loeysingList;
 
   if (loading) {
     return (
@@ -23,20 +23,32 @@ const KvalitetssikringApp = () => {
     );
   }
 
-  if (
-    typeof maaling === 'undefined' ||
-    typeof loesyingList === 'undefined' ||
-    loesyingList?.length === 0
+  const crawlResultat = maaling?.crawlResultat;
+  const loeysingCrawResultat = crawlResultat?.find(
+    (m) => String(m.loeysing.id) === loeysingId
+  );
+
+  if (typeof maaling === 'undefined') {
+    return <ErrorCard errorText="Maaling finnes ikkje" />;
+  } else if (
+    typeof crawlResultat === 'undefined' ||
+    crawlResultat?.length === 0
   ) {
-    return <ErrorCard show />;
+    return <ErrorCard errorText="Finner ikkje resultat etter sideutval" />;
+  } else if (typeof loeysingId === 'undefined') {
+    return <ErrorCard errorText="Løsysing eksisterar ikkje på måling" />;
+  } else if (typeof loeysingCrawResultat?.urlList === 'undefined') {
+    return (
+      <ErrorCard errorText="Sideutval på løsysing eksisterar ikkje på måling" />
+    );
   }
 
+  const crawlUrls: CrawlUrl[] = loeysingCrawResultat.urlList.map((url) => ({
+    url,
+  }));
+
   return (
-    <KvalitetssikringList
-      error={error}
-      loading={loading}
-      loeysingList={loesyingList}
-    />
+    <KvalitetssikringList error={error} loading={loading} urls={crawlUrls} />
   );
 };
 
