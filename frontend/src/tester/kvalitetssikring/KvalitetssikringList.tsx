bc462223374bb168ae721f25e49e6a18fef0e7b2
Tore Fotland <tore.jacobsen.fotland@digdir.no>
2023-03-09 14:11:49 +0100

    DFK-141 - [WIP] Lister urler fra crawling i Kvalitetssikring

--- frontend/src/tester/kvalitetssikring/KvalitetssikringList.tsx
+++ frontend/src/tester/kvalitetssikring/KvalitetssikringList.tsx
@@ -1,58 +1,53 @@
-import { ColumnDef } from '@tanstack/react-table';
-import React, { useMemo } from 'react';
+import { ColumnDef, Row } from '@tanstack/react-table';
+import React, { useCallback, useMemo } from 'react';
 
 import TestlabTable from '../../common/table/TestlabTable';
-import { Loeysing } from '../../loeysingar/api/types';
+import { ColumnUserAction } from '../../common/table/user-actions/UserActions';
+import { CrawlUrl } from '../../maaling/types';
 
 export interface Props {
   error: any;
   loading?: boolean;
-  loeysingList: Loeysing[];
+  urls: CrawlUrl[];
 }
 
-const KvalitetssikringList = ({ error, loading, loeysingList }: Props) => {
-  // const doStart = (loeysing: Loeysing) => {
-  //   console.log('starter test for ' + loeysing.url);
-  // };
-  //
-  // const onClickStart = useCallback((e: Row<Loeysing>) => {
-  //   doStart(e.original);
-  // }, []);
-  //
+const KvalitetssikringList = ({ error, loading, urls }: Props) => {
+  const doStart = (crawlUrl: CrawlUrl) => {
+    console.log('Fjerner ' + crawlUrl.url);
+  };
+
+  const onClickDelete = useCallback((e: Row<CrawlUrl>) => {
+    doStart(e.original);
+  }, []);
+
   // const onClickStartAll = useCallback((e: Table<Loeysing>) => {
   //   e.getCoreRowModel().rows.map((row) => {
   //     doStart(row.original);
   //   });
   // }, []);
-  //
-  // // const headerUserAction: HeaderUserAction = {
-  // //   startAllAction: onClickStartAll,
-  // // };
-  // const columnUserAction: ColumnUserAction = { startAction: onClickStart };
 
-  const loeysingColumns = useMemo<ColumnDef<Loeysing>[]>(
+  // const headerUserAction: HeaderUserAction = {
+  //   startAllAction: onClickStartAll,
+  // };
+  const columnUserAction: ColumnUserAction = { deleteAction: onClickDelete };
+
+  const urlColumns = useMemo<ColumnDef<CrawlUrl>[]>(
     () => [
       // {
       //   id: 'Handling',
-      //   cell: ({ row }) => <UserActions {...columnUserAction} row={row} />,
+      //   cell: ({ row }) => <UserActions {...columnUserAction} row={row}/>,
       //   enableSorting: false,
-      //   header: ({ table }) => (
-      //     <span>
-      //       <UserTableActions {...headerUserAction} table={table} />
-      //     </span>
-      //   ),
+      //   // header: ({ table }) => (
+      //   //   <span>
+      //   //     <UserTableActions {...headerUserAction} table={table} />
+      //   //   </span>
+      //   // ),
       //   size: 1,
       // },
       {
-        accessorFn: (row) => row.namn,
-        id: 'Navn',
-        cell: (info) => info.getValue(),
-        header: () => <span>Navn</span>,
-      },
-      {
         accessorFn: (row) => row.url,
         id: 'url',
-        cell: (info) => info.getValue(),
+        cell: ({ row }) => <a href={row.original.url}>{row.original.url}</a>,
         header: () => <span>URL</span>,
       },
     ],
@@ -60,9 +55,9 @@ const KvalitetssikringList = ({ error, loading, loeysingList }: Props) => {
   );
 
   return (
-    <TestlabTable<Loeysing>
-      data={loeysingList}
-      defaultColumns={loeysingColumns}
+    <TestlabTable<CrawlUrl>
+      data={urls}
+      defaultColumns={urlColumns}
       error={error}
       loading={loading}
     />
