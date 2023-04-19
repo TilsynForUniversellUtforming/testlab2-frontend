23e7027c8796f59a9492d400212363e62282be85
Tore Fotland <tore.jacobsen.fotland@digdir.no>
2023-04-19 16:00:24 +0200

    DFK-209 - Fjernet IndexController - Oppdatert SPAConfig til å kun cache andre filer enn index.html. Cache satt til 7 dager - Lagt til favicon

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/SPAConfig.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/SPAConfig.kt
@@ -1,87 +1,44 @@
 package no.uutilsynet.testlab2frontendserver
 
-import jakarta.servlet.http.HttpServletRequest
-import java.io.IOException
+import java.time.Duration
+import java.util.concurrent.TimeUnit
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.Resource
-import org.springframework.util.StringUtils
+import org.springframework.http.CacheControl
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import org.springframework.web.servlet.resource.ResourceResolver
-import org.springframework.web.servlet.resource.ResourceResolverChain
+import org.springframework.web.servlet.resource.PathResourceResolver
 
 @Configuration
 class SinglePageAppConfig : WebMvcConfigurer {
 
+  val resolver =
+      object : PathResourceResolver() {
+        override fun getResource(resourcePath: String, location: Resource): Resource {
+          val requestedResource = location.createRelative(resourcePath)
+          return if (requestedResource.exists() && requestedResource.isReadable) {
+            requestedResource
+          } else ClassPathResource("/public/index.html")
+        }
+      }
+
   override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
     registry
+        .setOrder(2)
+        .addResourceHandler("/index.html")
+        .addResourceLocations("classpath:/public/")
+        .setCacheControl(CacheControl.maxAge(0, TimeUnit.SECONDS).mustRevalidate())
+        .setCacheControl(CacheControl.noCache())
+        .resourceChain(true)
+        .addResolver(resolver)
+
+    registry
+        .setOrder(3)
         .addResourceHandler("/**")
         .addResourceLocations("classpath:/public/")
-        .resourceChain(false)
-        .addResolver(PushStateResourceResolver())
-  }
-
-  private inner class PushStateResourceResolver : ResourceResolver {
-    private val index: Resource = ClassPathResource("/public/index.html")
-    private val handledExtensions =
-        listOf(
-            "html",
-            "js",
-            "json",
-            "csv",
-            "css",
-            "png",
-            "svg",
-            "eot",
-            "ttf",
-            "woff",
-            "appcache",
-            "jpg",
-            "jpeg",
-            "gif",
-            "ico")
-    private val ignoredPaths = listOf("api")
-
-    override fun resolveResource(
-        request: HttpServletRequest?,
-        requestPath: String,
-        locations: List<Resource>,
-        chain: ResourceResolverChain
-    ): Resource? {
-      return resolve(requestPath, locations)
-    }
-
-    override fun resolveUrlPath(
-        resourcePath: String,
-        locations: List<Resource>,
-        chain: ResourceResolverChain
-    ): String? {
-      return resolve(resourcePath, locations)?.let { resolvedResource ->
-        try {
-          resolvedResource.url.toString()
-        } catch (e: IOException) {
-          resolvedResource.filename
-        }
-      }
-    }
-
-    private fun resolve(requestPath: String, locations: List<Resource>): Resource? {
-      if (requestPath in ignoredPaths) {
-        return null
-      }
-      return if (isHandled(requestPath)) {
-        locations.firstNotNullOfOrNull { loc ->
-          loc.createRelative(requestPath).takeIf { it.exists() }
-        }
-      } else {
-        index
-      }
-    }
-
-    private fun isHandled(path: String): Boolean {
-      val extension = StringUtils.getFilenameExtension(path)
-      return handledExtensions.contains(extension)
-    }
+        .setCachePeriod(Duration.ofDays(7).seconds.toInt())
+        .resourceChain(true)
+        .addResolver(resolver)
   }
 }
