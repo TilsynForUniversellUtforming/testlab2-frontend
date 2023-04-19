6a9a63c5065b296bd33140f46217f2770ebf8c94
Tore Fotland <tore.jacobsen.fotland@digdir.no>
2023-04-19 13:17:46 +0200

    DFK-209 - IndexController for root

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/IndexController.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/IndexController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("api/v1")
 class IndexController {
   @GetMapping
   fun getIndex(response: HttpServletResponse) {
