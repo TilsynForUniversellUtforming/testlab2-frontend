acefdec5e1a19ec5311a8bc4177469b6b1e0d2d2
Tore Fotland <tore.jacobsen.fotland@digdir.no>
2023-04-19 13:05:09 +0200

    DFK-209 - IndexController for root

---
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/IndexController.kt
@@ -0,0 +1,16 @@
+package no.uutilsynet.testlab2frontendserver
+
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/")
+class IndexController {
+  @GetMapping
+  fun getIndex(response: HttpServletResponse) {
+    response.setHeader(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, max-age=0, must-revalidate")
+  }
+}
