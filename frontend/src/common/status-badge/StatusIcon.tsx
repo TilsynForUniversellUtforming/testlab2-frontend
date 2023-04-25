ef40d2754409c98ef8f53095ded440e63a55b802
Tore Fotland <tore.jacobsen.fotland@digdir.no>
2023-04-25 15:18:51 +0200

    DFK-223 - [BUGFIX] Viser listen selv med tomt testresultat - [BUGFIX] Fikset status (ikon, statistikk) for oversiktsbildet for måling - Lagt til ikon for feilet og byttet til bruk av Digdir-ikoner

--- frontend/src/common/status-badge/StatusIcon.tsx
+++ frontend/src/common/status-badge/StatusIcon.tsx
@@ -1,17 +1,29 @@
 import './status-icon.scss';
 
+import { AlertTriangleIcon, CheckIcon, CloseIcon } from '@digdir/ds-icons';
+import classNames from 'classnames';
 import React from 'react';
 
 export interface Props {
   finished?: boolean;
+  error?: boolean;
 }
 
-const Checkmark = () => <>&#10003;</>;
-const Cross = () => <>&#10005;</>;
+const Icon = ({ finished, error }: Props) => {
+  if (error) {
+    return <AlertTriangleIcon color={'var(--colors-red-600)'} />;
+  } else if (finished) {
+    return <CheckIcon color={'var(--colors-green-700)'} />;
+  } else {
+    return <CloseIcon color={'var(--colors-grey-600)'} />;
+  }
+};
 
-const StatusIcon = ({ finished = false }: Props) => (
-  <div className="status-icon" style={{ height: '2rem', width: '2rem' }}>
-    {finished ? <Checkmark /> : <Cross />}
+const StatusIcon = ({ finished = false, error = false }: Props) => (
+  <div
+    className={classNames('status-icon', { error: error, finished: finished })}
+  >
+    <Icon finished={finished} error={error} />
   </div>
 );
 
