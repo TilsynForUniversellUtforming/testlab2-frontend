7c6a2f813aedff7ecf9642102e5bbe9d9a1b378e
Tore Fotland <tore.jacobsen.fotland@digdir.no>
2023-06-06 14:41:43 +0200

    DFK-89 - Flyttet ut kolonner og bruker UserActionTable for liste av tester og testresultat - Lagt til id på checkboxes i tabell - [BUGFIX] Riktig decoding av base64

---
+++ frontend/src/tester/test-result-list/TestResultatColumns.tsx
@@ -0,0 +1,64 @@
+import { ColumnDef } from '@tanstack/react-table';
+import React from 'react';
+
+import { TestResultat } from '../api/types';
+
+const decodeBase64 = (base64String?: string) => {
+  if (typeof base64String === 'undefined') {
+    return '';
+  }
+
+  try {
+    return decodeURIComponent(
+      atob(base64String)
+        .split('')
+        .map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join('')
+    );
+  } catch (e) {
+    return 'Feil ved dekoding';
+  }
+};
+
+/**
+ * getTestresultatColumns function returns an array of column definitions for TestResultat.
+ *
+ * @returns {Array<ColumnDef<TestResultat>>} An array of column definitions.
+ */
+export const getTestresultatColumns = (): Array<ColumnDef<TestResultat>> => [
+  {
+    accessorFn: (row) => row.testregelId,
+    id: '_idTestregel',
+    cell: (info) => info.getValue(),
+    header: () => <>Testregel</>,
+  },
+  {
+    accessorFn: (row) => row.suksesskriterium.join(', '),
+    id: '_idSuksesskriterium',
+    cell: ({ row }) => <>{row.original.suksesskriterium.join(', ')}</>,
+    header: () => <>Suksesskriterium</>,
+  },
+  {
+    accessorFn: (row) => row.side,
+    id: 'side',
+    cell: (info) => (
+      <a href={String(info.getValue())}>{String(info.getValue())}</a>
+    ),
+    header: () => <>Nettside</>,
+  },
+
+  {
+    accessorFn: (row) => row.elementOmtale?.htmlCode,
+    id: 'htmlCode',
+    cell: (info) => <>{decodeBase64(String(info.getValue()))}</>,
+    header: () => <>HTML element</>,
+  },
+  {
+    accessorFn: (row) => row.elementOmtale?.pointer,
+    id: 'pointer',
+    cell: (info) => info.getValue(),
+    header: () => <>Peker</>,
+  },
+];
---
+++ frontend/src/tester/testing-list/TestingListColumns.tsx
@@ -0,0 +1,75 @@
+import { ColumnDef } from '@tanstack/react-table';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { appRoutes, getFullPath, idPath } from '../../common/appRoutes';
+import StatusBadge from '../../common/status-badge/StatusBadge';
+import { TestResult } from '../../maaling/api/types';
+
+/**
+ * getTestingListColumns function returns an array of column definitions for TestResult.
+ *
+ * @param {string} maalingId The id of the maaling.
+ *
+ * @returns {Array<ColumnDef<TestResult>>} An array of column definitions.
+ */
+export const getTestingListColumns = (
+  maalingId?: string
+): Array<ColumnDef<TestResult>> => [
+  {
+    accessorFn: (row) => row.loeysing.namn,
+    id: 'url',
+    cell: ({ row }) =>
+      maalingId ? (
+        <Link
+          to={getFullPath(
+            appRoutes.TEST_RESULT_LIST,
+            { pathParam: idPath, id: maalingId },
+            {
+              pathParam: ':loeysingId',
+              id: String(row.original.loeysing.id),
+            }
+          )}
+        >
+          {row.original.loeysing.namn}
+        </Link>
+      ) : (
+        String(row.original.loeysing.id)
+      ),
+    header: () => <>Løysing</>,
+  },
+  {
+    accessorFn: (row) => row.loeysing.namn,
+    id: 'namn',
+    cell: (info) => info.getValue(),
+    header: () => <>Verksemd</>,
+  },
+  {
+    accessorFn: (row) => row.tilstand,
+    id: 'status',
+    cell: ({ row }) => {
+      const status = String(row.original.tilstand);
+      const fremgang = row.original.framgang;
+      let label: string | undefined = undefined;
+      if (status === 'starta' && typeof fremgang !== 'undefined') {
+        label = `Tester ${fremgang.prosessert} av ${fremgang.maxLenker}`;
+      }
+
+      return (
+        <StatusBadge
+          status={status}
+          customLabel={label}
+          levels={{
+            primary: ['starta', 'ikkje_starta'],
+            danger: ['feila'],
+            success: ['ferdig'],
+          }}
+        />
+      );
+    },
+    header: () => <>Status</>,
+    meta: {
+      select: true,
+    },
+  },
+];
