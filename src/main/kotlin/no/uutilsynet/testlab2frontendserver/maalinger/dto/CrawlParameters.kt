b0ab949dc740ff63b819f148ba81f3a2d8cf91ca
Lars Lønne <lonnelars@gmail.com>
2023-08-02 14:00:12 +0200

    flytter validering til testlab2-testing

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/maalinger/dto/CrawlParameters.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/maalinger/dto/CrawlParameters.kt
@@ -1,16 +1,3 @@
 package no.uutilsynet.testlab2frontendserver.maalinger.dto
 
-data class CrawlParameters(val maxLinksPerPage: Int = 100, val numLinksToSelect: Int = 30) {
-  companion object {
-    fun CrawlParameters.validateParameters(): CrawlParameters =
-        if (maxLinksPerPage > 2000) {
-          throw IllegalArgumentException("Kan ikkje velgje meir enn 2000 sidar per løysing")
-        } else if (numLinksToSelect > 2000) {
-          throw IllegalArgumentException("Kan ikkje velgje større utval enn 2000 sidar per løysing")
-        } else if (maxLinksPerPage < 10) {
-          throw IllegalArgumentException("Kan ikkje velgje mindre enn 10 sidar per løysing")
-        } else if (numLinksToSelect < 10) {
-          throw IllegalArgumentException("Kan ikkje velgje mindre utval enn 10 sidar per løysing")
-        } else this
-  }
-}
+data class CrawlParameters(val maxLinksPerPage: Int = 100, val numLinksToSelect: Int = 30)
