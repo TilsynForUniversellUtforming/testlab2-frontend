ec4cf85f3cead5e41ccf3c397bdd463ec1abe187
torefotland <121802694+torefotland@users.noreply.github.com>
2023-08-02 15:05:36 +0200

    DFK-283 (#29)
    - Henter detaljert feilmelding fra HttpClientErrorException
    - [BUGFIX] responseToJson returnerer riktig feilmeldinger

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/common/RestHelper.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/common/RestHelper.kt
@@ -1,6 +1,7 @@
 package no.uutilsynet.testlab2frontendserver.common
 
 import org.springframework.core.ParameterizedTypeReference
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForEntity
 
@@ -10,4 +11,10 @@ object RestHelper {
 
   inline fun <reified T : Any> RestTemplate.getList(url: String): List<T> =
       (getForEntity<Array<T>>(url, typeRef<Array<T>>()).body ?: emptyArray()).toList()
+
+  fun getDetailedErrorMessage(exception: HttpClientErrorException): String {
+    val regex = Regex("\"(.*)\"")
+    val result = regex.find(exception.message.orEmpty())?.groupValues?.get(1)
+    return result ?: ""
+  }
 }
