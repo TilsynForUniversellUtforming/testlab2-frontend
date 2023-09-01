6c5a75a6d72805495ea52e245b0b6c0f3024b7ef
oho <oystein.hoyum@digdir.no>
2023-09-01 13:02:23 +0200

    DFK-252 Krav om rolle for tilgang

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/Testlab2FrontendServerApplication.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/Testlab2FrontendServerApplication.kt
@@ -1,13 +1,14 @@
 package no.uutilsynet.testlab2frontendserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.web.client.RestTemplate
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 @ConfigurationPropertiesScan
 class Testlab2FrontendServerApplication(val restTemplateBuilder: RestTemplateBuilder) {
   @Bean fun restTemplate(): RestTemplate = restTemplateBuilder.build()
