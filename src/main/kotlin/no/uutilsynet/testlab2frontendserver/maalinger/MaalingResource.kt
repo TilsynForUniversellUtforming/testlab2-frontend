b8d72d48e2fa85245e0c0bc95e1fe9a88974b526
oho <oystein.hoyum@digdir.no>
2023-09-01 13:02:23 +0200

    DFK-252 Krav om rolle for tilgang

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/maalinger/MaalingResource.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/maalinger/MaalingResource.kt
@@ -23,15 +23,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
 
@@ -234,4 +226,10 @@ class MaalingResource(
             logger.error("Feila ved henting av testreglar for måling $maalingId", it)
             throw RuntimeException("Klarte ikkje å hente testreglar")
           }
+
+  @GetMapping("/testHeaders")
+  fun appheaders(@RequestHeader allHeaders: Map<String, String>): String {
+    println("Headers " + allHeaders)
+    return allHeaders.toString()
+  }
 }
