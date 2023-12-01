1f02b8de54e5576f05c9487fa95a4915217ee2c0
oho <oystein.hoyum@digdir.no>
2023-12-01 14:36:03 +0100

    DFK-252 Justert config for fiks av testoppsett

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/common/RestTemplateHeaderModifierInterceptor.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/common/RestTemplateHeaderModifierInterceptor.kt
@@ -4,10 +4,15 @@ import org.springframework.http.HttpRequest
 import org.springframework.http.client.ClientHttpRequestExecution
 import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.http.client.ClientHttpResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.stereotype.Service
 
 @Service
-class RestTemplateHeaderModifierInterceptor(val securityHelper: SecurityHelper) :
+class RestTemplateHeaderModifierInterceptor(val clientService: OAuth2AuthorizedClientService) :
     ClientHttpRequestInterceptor {
 
   override fun intercept(
@@ -15,8 +20,17 @@ class RestTemplateHeaderModifierInterceptor(val securityHelper: SecurityHelper) 
       body: ByteArray,
       execution: ClientHttpRequestExecution
   ): ClientHttpResponse {
-    val accessToken = securityHelper.getAccessToken()
-    request.headers.set("Authorization", "Bearer " + accessToken)
-    return execution.execute(request, body)
+    val authentication: Authentication = SecurityContextHolder.getContext().authentication
+    if (authentication.isAuthenticated && authentication is OAuth2AuthenticationToken) {
+      val client: OAuth2AuthorizedClient =
+          clientService.loadAuthorizedClient(
+              authentication.getAuthorizedClientRegistrationId(), authentication.name)
+      val accessToken = client.accessToken.tokenValue
+      request.headers.set("Authorization", "Bearer " + accessToken)
+      println("AccessToken " + accessToken + "request " + request.uri)
+      return execution.execute(request, body)
+    } else {
+      return execution.execute(request, body)
+    }
   }
 }
