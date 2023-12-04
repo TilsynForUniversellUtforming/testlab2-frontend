c9e3cf4fd987c9c4d20b1d74a91298942e35c5d6
oho <oystein.hoyum@digdir.no>
2023-12-04 14:25:00 +0100

    DFK-420 Refresh-token bruk i restTemplate

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/Testlab2FrontendServerApplication.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/Testlab2FrontendServerApplication.kt
@@ -1,22 +1,28 @@
 package no.uutilsynet.testlab2frontendserver
 
+import no.uutilsynet.testlab2frontendserver.common.BearerTokenInterceptor
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
+import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 @ConfigurationPropertiesScan
 class Testlab2FrontendServerApplication(
     val restTemplateBuilder: RestTemplateBuilder,
+    val bearerTokenInterceptor: BearerTokenInterceptor
 ) {
 
   @Bean
   fun restTemplate(): RestTemplate {
-    return restTemplateBuilder.build()
+    val interceptors: ArrayList<ClientHttpRequestInterceptor> = ArrayList()
+    interceptors.add(bearerTokenInterceptor)
+
+    return restTemplateBuilder.interceptors(interceptors).build()
   }
 }
 
---
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/common/BearerTokenInterceptor.kt
@@ -0,0 +1,80 @@
+package no.uutilsynet.testlab2frontendserver.common
+
+import java.io.IOException
+import java.time.Clock
+import org.springframework.http.HttpRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.client.ClientHttpRequestExecution
+import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.http.client.ClientHttpResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
+import org.springframework.security.oauth2.core.AbstractOAuth2Token
+import org.springframework.stereotype.Component
+
+@Component
+class BearerTokenInterceptor(
+    val clientService: OAuth2AuthorizedClientService,
+    val clientManager: OAuth2AuthorizedClientManager
+) : ClientHttpRequestInterceptor {
+
+  @Throws(IOException::class)
+  override fun intercept(
+      request: HttpRequest,
+      bytes: ByteArray,
+      execution: ClientHttpRequestExecution
+  ): ClientHttpResponse {
+    val authentication: Authentication = SecurityContextHolder.getContext().authentication
+
+    return if (authentication is OAuth2AuthenticationToken) {
+      val oauthToken = authentication
+      val clientRegistrationId = oauthToken.authorizedClientRegistrationId
+      var client =
+          clientService.loadAuthorizedClient<OAuth2AuthorizedClient>(
+              clientRegistrationId, oauthToken.name)
+      if (client == null) {
+        client = reauthorize(oauthToken)
+      }
+      if (hasTokenExpired(
+          client!!
+              .accessToken)) { // access_token expired - re-authorize to get a fresh access_token
+        client = reauthorize(oauthToken)
+      }
+      request.getHeaders().set("Authorization", "Bearer " + client!!.accessToken.tokenValue)
+      var response: ClientHttpResponse = execution.execute(request, bytes)
+      if (HttpStatus.UNAUTHORIZED ==
+          response.getStatusCode()) { // token might have been revoked - re-authorize and try again
+        client = reauthorize(oauthToken)
+        request.getHeaders().set("Authorization", "Bearer " + client!!.accessToken.tokenValue)
+        response = execution.execute(request, bytes)
+      }
+      response
+    } else {
+      execution.execute(request, bytes)
+    }
+  }
+
+  private fun reauthorize(oauthToken: OAuth2AuthenticationToken): OAuth2AuthorizedClient? {
+    val clientRegistrationId = oauthToken.authorizedClientRegistrationId
+    val authzRequest =
+        OAuth2AuthorizeRequest.withClientRegistrationId(clientRegistrationId)
+            .principal(oauthToken)
+            .build()
+    val reauthorizedClient =
+        clientManager.authorize(authzRequest) // re-authorize with refresh_token
+    if (reauthorizedClient != null) {
+      clientService.saveAuthorizedClient(reauthorizedClient, oauthToken)
+    }
+    return reauthorizedClient
+  }
+
+  private val clock = Clock.systemUTC()
+  private fun hasTokenExpired(token: AbstractOAuth2Token): Boolean {
+    return clock.instant().isAfter(token.expiresAt?.minusSeconds(10) ?: return false)
+  }
+}
