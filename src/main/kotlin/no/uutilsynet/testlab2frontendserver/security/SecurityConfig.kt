459e3a2ea83a1f341beff6f4f077c00f6fc6559d
oho <oystein.hoyum@digdir.no>
2023-12-21 10:24:29 +0100

    DFK-252 csrf opprydding

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/security/SecurityConfig.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/security/SecurityConfig.kt
@@ -1,11 +1,5 @@
 package no.uutilsynet.testlab2frontendserver
 
-import jakarta.servlet.FilterChain
-import jakarta.servlet.ServletException
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
-import java.io.IOException
-import java.util.function.Supplier
 import java.util.stream.Collectors
 import no.uutilsynet.testlab2securitylib.RoleExtractor
 import org.springframework.context.annotation.Bean
@@ -17,14 +11,10 @@ import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper
 import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 import org.springframework.security.web.csrf.*
-import org.springframework.util.StringUtils
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 @EnableWebSecurity
@@ -34,16 +24,11 @@ class SecurityConfig {
   @Bean
   open fun filterChain(http: HttpSecurity): SecurityFilterChain {
     http {
-      csrf {
-        csrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse()
-        //    csrfTokenRequestHandler = SpaCsrfTokenRequestHandler()
-        sessionAuthenticationStrategy = NullAuthenticatedSessionStrategy()
-      }
+      csrf { csrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse() }
       authorizeHttpRequests { authorize(anyRequest, hasAuthority("brukar subscriber")) }
       oauth2Login { userInfoEndpoint { userAuthoritiesMapper = userAuthoritiesMapper() } }
       cors { configurationSource = corsConfigurationSource() }
     }
-    http.addFilterAfter(CsrfCookieFilter(), BasicAuthenticationFilter::class.java)
 
     return http.build()
   }
@@ -63,13 +48,6 @@ class SecurityConfig {
     return source
   }
 
-  //  @Value("\${spring.websecurity.debug:false}") var webSecurityDebug = true
-  //
-  //  @Bean
-  //  fun webSecurityCustomizer(): WebSecurityCustomizer? {
-  //    return WebSecurityCustomizer { web: WebSecurity -> web.debug(webSecurityDebug) }
-  //  }
-
   private fun userAuthoritiesMapper(): GrantedAuthoritiesMapper =
       GrantedAuthoritiesMapper { authorities: Collection<GrantedAuthority> ->
         val roles: Set<GrantedAuthority> =
@@ -85,59 +63,4 @@ class SecurityConfig {
 
         roles
       }
-}
-
-class SpaCsrfTokenRequestHandler : CsrfTokenRequestAttributeHandler() {
-  private val delegate: CsrfTokenRequestHandler = XorCsrfTokenRequestAttributeHandler()
-
-  override fun handle(
-      request: HttpServletRequest,
-      response: HttpServletResponse,
-      csrfToken: Supplier<CsrfToken>
-  ) {
-    /*
-     * Always use XorCsrfTokenRequestAttributeHandler to provide BREACH protection of
-     * the CsrfToken when it is rendered in the response body.
-     */
-    delegate.handle(request, response, csrfToken)
-  }
-
-  override fun resolveCsrfTokenValue(request: HttpServletRequest, csrfToken: CsrfToken): String {
-    /*
-     * If the request contains a request header, use CsrfTokenRequestAttributeHandler
-     * to resolve the CsrfToken. This applies when a single-page application includes
-     * the header value automatically, which was obtained via a cookie containing the
-     * raw CsrfToken.
-     */
-    println(request.headerNames.toList())
-    println(csrfToken.headerName)
-    println(csrfToken.token)
-    println(request.getHeader(csrfToken.headerName))
-    return if (StringUtils.hasText(request.getHeader(csrfToken.headerName))) {
-      super.resolveCsrfTokenValue(request, csrfToken)
-    } else {
-      /*
-       * In all other cases (e.g. if the request contains a request parameter), use
-       * XorCsrfTokenRequestAttributeHandler to resolve the CsrfToken. This applies
-       * when a server-side rendered form includes the _csrf request parameter as a
-       * hidden input.
-       */
-      delegate.resolveCsrfTokenValue(request, csrfToken)
-    }
-  }
-}
-
-class CsrfCookieFilter : OncePerRequestFilter() {
-
-  @Throws(ServletException::class, IOException::class)
-  override fun doFilterInternal(
-      request: HttpServletRequest,
-      response: HttpServletResponse,
-      filterChain: FilterChain
-  ) {
-    val csrfToken = request.getAttribute("_csrf") as CsrfToken
-    // Render the token value to a cookie by causing the deferred token to be loaded
-    csrfToken.token
-    filterChain.doFilter(request, response)
-  }
 }
