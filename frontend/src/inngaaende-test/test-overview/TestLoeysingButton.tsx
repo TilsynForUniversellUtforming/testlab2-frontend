7bb445ea9beb4660e88f25a9326b8a61dd8d1304
Tore Fotland <tore.jacobsen.fotland@digdir.no>
2024-01-09 15:18:34 +0100

    DFK-434 - DFK-437 - [WIP] Oversikt over løsninger som inngår i en test

--- frontend/src/inngaaende-test/test-overview/TestLoeysingButton.tsx
+++ frontend/src/inngaaende-test/test-overview/TestLoeysingButton.tsx
@@ -7,18 +7,25 @@ interface Props {
 
 const TestLoeysingButton = ({ name, onClick }: Props) => (
   <div className="manual-test__loeysing-button">
-    <div className="manual-test__loeysing-button-tag">
-      <Tag color="first" size="small" variant="secondary">
-        Ikke startet
-      </Tag>
+    <div className="tag-wrapper">
+      <div>
+        <Tag color="first" size="small" variant="secondary">
+          Ikkje startet
+        </Tag>
+      </div>
     </div>
-    <div className="manual-test__loeysing-button-content">
-      <Heading size="medium" level={4}>
-        {name}
-      </Heading>
-      <Tag color="second" size="medium" variant="secondary">
-        Inngående kontroll
-      </Tag>
+    <div className="content-wrapper">
+      <div className="content">
+        <Heading size="medium" level={4} spacing>
+          {name}
+        </Heading>
+        <Tag color="second" size="small" variant="secondary">
+          Inngående kontroll
+        </Tag>
+        <Tag color="info" size="small" variant="secondary">
+          Nettsted
+        </Tag>
+      </div>
       <Button title="Start testing" onClick={onClick}>
         Start testing
       </Button>
