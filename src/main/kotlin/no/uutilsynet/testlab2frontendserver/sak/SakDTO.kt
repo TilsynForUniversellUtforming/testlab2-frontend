0fce1da794a2572d1d9cf7d3e05a75f04f9623d4
Lars Lønne <lars.lonne@kantega.no>
2024-01-18 14:05:00 +0100

    utvider SakDTO med frist
    DFK-438

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/sak/SakDTO.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/sak/SakDTO.kt
@@ -1,11 +1,13 @@
 package no.uutilsynet.testlab2frontendserver.sak
 
+import java.time.LocalDate
 import no.uutilsynet.testlab2frontendserver.testreglar.dto.Testregel
 
 data class SakDTO(
     val id: Int,
     val namn: String,
     val virksomhet: String,
+    val frist: LocalDate,
     val loeysingar: List<SakLoeysingDTO> = emptyList(),
     val testreglar: List<Testregel> = emptyList()
 ) {
