1100fb15de4572f557547ef050561493e1176e4b
oho <oystein.hoyum@digdir.no>
2024-01-31 16:40:45 +0100

    DFK-464 Basis visning av resultat frå backend

---
+++ frontend/src/resultat/ResultatRoutes.tsx
@@ -0,0 +1,31 @@
+import { AppRoute, idPath } from '@common/util/routeUtils';
+import React from 'react';
+import { Outlet, RouteObject } from 'react-router-dom';
+
+import resultatImg from '../assets/resultat.svg';
+import TestResultatApp from './TestResultatApp';
+
+export const RESULTAT_ROOT: AppRoute = {
+  navn: 'Resultat',
+  path: 'resultat/',
+  imgSrc: resultatImg,
+};
+
+export const TESTRESULTAT_TESTGRUNNLAG: AppRoute = {
+  navn: 'Resultat testgrunnlag',
+  path: idPath,
+  parentRoute: RESULTAT_ROOT,
+};
+
+export const ResultRoutes: RouteObject = {
+  path: RESULTAT_ROOT.path,
+  element: <Outlet />,
+  handle: { name: RESULTAT_ROOT.navn },
+  children: [
+    {
+      path: TESTRESULTAT_TESTGRUNNLAG.path,
+      element: <TestResultatApp />,
+      handle: { name: TESTRESULTAT_TESTGRUNNLAG.navn },
+    },
+  ],
+};
