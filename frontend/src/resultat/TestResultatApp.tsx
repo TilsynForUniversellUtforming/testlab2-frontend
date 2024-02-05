eb448cf1bd758bfb15ef07c24cb1d53b90157d05
oho <oystein.hoyum@digdir.no>
2024-02-05 11:53:39 +0100

    DFK-464 Refaktorering

--- frontend/src/resultat/TestResultatApp.tsx
+++ frontend/src/resultat/TestResultatApp.tsx
@@ -1,57 +1,15 @@
-import ErrorCard from '@common/error/ErrorCard';
-import { useEffectOnce } from '@common/hooks/useEffectOnce';
 import UserActionTable from '@common/table/UserActionTable';
-import { withErrorHandling } from '@common/util/apiUtils';
 import { AggregatedTestresult } from '@maaling/api/types';
 import { getAggregatedResultColumns } from '@maaling/resultat/testing-list/test-result-list/TestResultColumns';
-import React, { useCallback, useMemo, useState } from 'react';
-import { useParams } from 'react-router-dom';
-
-import { fetchTestresultatAggregert } from './resultat-api';
+import React, { useMemo } from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
 
 const TestResultatApp = () => {
   const { id } = useParams();
-  const [testResultList, setTestResultList] = useState<AggregatedTestresult[]>(
-    []
-  );
+
+  const testResultList: AggregatedTestresult[] =
+    useLoaderData() as AggregatedTestresult[];
   const testResultatColumns = useMemo(() => getAggregatedResultColumns(), []);
-
-  const [error, setError] = useState<Error | undefined>();
-
-  useEffectOnce(() => {
-    doFetchTestresultat().then((testresultatAggregert) => {
-      setTestResultList(testresultatAggregert);
-    });
-  });
-
-  const doFetchTestresultat = useCallback(
-    withErrorHandling(
-      async () => {
-        if (id) {
-          const testresultatAggregert = await fetchTestresultatAggregert(
-            Number(id)
-          );
-
-          return testresultatAggregert;
-        } else {
-          throw new Error('Fant ikkje testresultat med id $id');
-        }
-      },
-      'Kunne ikkje hente måling',
-      setError
-    ),
-    [id]
-  );
-
-  if (error) {
-    return (
-      <ErrorCard
-        errorHeader={error.name}
-        error={error}
-        buttonText={error.name ?? 'Prøv igjen'}
-      />
-    );
-  }
 
   return (
     <UserActionTable<AggregatedTestresult>
--- frontend/src/resultat/resultat-api.ts
+++ frontend/src/resultat/resultat-api.ts
@@ -4,8 +4,6 @@ import { TestResult } from '@maaling/api/types';
 export const fetchTestresultatAggregert = async (
   id: number
 ): Promise<TestResult> =>
-  fetch(`/api/v1/testresultat/aggregert/${id}`, {
-    method: 'GET',
-  }).then((response) =>
+  fetch(`/api/v1/testresultat/aggregert/${id}`, {}).then((response) =>
     responseToJson(response, 'Kunne ikkje hente for loeysing')
   );
