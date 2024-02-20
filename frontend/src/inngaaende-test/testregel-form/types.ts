039510ebcc70149dea371c9c6902b57187961708
Lars Lønne <lonnelars@gmail.com>
2024-02-20 14:48:53 +0100

    hver testregel kan ha en liste med resultater
    Endrer input til TestForm fra ett resultat, til en liste med resultater. Det legger
    til rette for at vi kan ha flere resultater for hver testregel, sånn at vi kan teste
    flere elementer.
    
    DFK-483

--- frontend/src/inngaaende-test/testregel-form/types.ts
+++ frontend/src/inngaaende-test/testregel-form/types.ts
@@ -1,13 +1,7 @@
 import { Svar } from '@test/api/types';
 import { TestregelSkjema } from '@test/util/testregelParser';
 
-export type TestFormStep = {
-  key: string;
-  utfall?: string;
-  fasit?: string;
-};
-
-export type SkjemaOgSvar = {
-  skjemaModell: TestregelSkjema;
-  alleSvar: Svar[];
+export type SkjemaMedSvar = {
+  skjema: TestregelSkjema;
+  svar: Svar[];
 };
