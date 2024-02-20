b125eb7ebc9a9434db4423884b6cdd603705fc2f
Lars Lønne <lonnelars@gmail.com>
2024-02-20 12:39:07 +0100

    fikser dropdown i toppmenyen

--- frontend/src/common/dropdown/NavigationLinksDropdown.tsx
+++ frontend/src/common/dropdown/NavigationLinksDropdown.tsx
@@ -4,7 +4,7 @@ import { ButtonColor, ButtonSize, ButtonVariant } from '@common/types';
 import { Button, DropdownMenu } from '@digdir/design-system-react';
 import { ChevronDownIcon } from '@navikt/aksel-icons';
 import classNames from 'classnames';
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { AppRoute, getFullPath } from '../util/routeUtils';
@@ -17,7 +17,6 @@ interface Props {
 export const NavigationLinksDropdown = ({ navn, routes }: Props) => {
   const [show, setShow] = useState(false);
   const navigate = useNavigate();
-  const anchorEl = useRef(null);
 
   const handleButtonClick = (route: AppRoute) => {
     setShow(false);
@@ -28,30 +27,30 @@ export const NavigationLinksDropdown = ({ navn, routes }: Props) => {
   };
 
   return (
-    <div className="dropdown">
-      <Button
-        className={classNames('dropdown__button', { show: show })}
-        onClick={() => {
-          setShow((show) => !show);
-        }}
-        variant={ButtonVariant.Quiet}
-        color={ButtonColor.Inverted}
-        ref={anchorEl}
-        aria-haspopup="true"
-        aria-expanded={show}
-        id={navn}
+    <div className={classNames('dropdown-content links', { show })}>
+      <DropdownMenu
+        open={show}
+        onClose={() => setShow(false)}
+        size={ButtonSize.Small}
+        placement="bottom-start"
       >
-        {navn}
-        <ChevronDownIcon className="chevron-icon" />
-      </Button>
-      <div className={classNames('dropdown-content links', { show })}>
-        <DropdownMenu
-          anchorEl={anchorEl.current}
-          open={show}
-          onClose={() => setShow(false)}
-          size={ButtonSize.Small}
-          placement="bottom"
-        >
+        <DropdownMenu.Trigger asChild={true}>
+          <Button
+            className={classNames('dropdown__button', { show: show })}
+            onClick={() => {
+              setShow((show) => !show);
+            }}
+            variant={ButtonVariant.Quiet}
+            color={ButtonColor.Inverted}
+            aria-haspopup="true"
+            aria-expanded={show}
+            id={navn}
+          >
+            {navn}
+            <ChevronDownIcon className="chevron-icon" />
+          </Button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
           <DropdownMenu.Group>
             {routes.map((route) => {
               if (route.disabled) {
@@ -72,8 +71,8 @@ export const NavigationLinksDropdown = ({ navn, routes }: Props) => {
               );
             })}
           </DropdownMenu.Group>
-        </DropdownMenu>
-      </div>
+        </DropdownMenu.Content>
+      </DropdownMenu>
     </div>
   );
 };
