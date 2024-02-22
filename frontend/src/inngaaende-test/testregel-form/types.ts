ab6f1fc361b445bf74d61cbb8fd9f49a46537fc9
Lars Lønne <lonnelars@gmail.com>
2024-02-22 13:00:13 +0100

    accordion for testregelskjemaer
    Lager en accordion for testregelskjemaer, sånn at det er mulig å vise flere resultater under samme testregel. Når det bare er ett resultat, vises ikke accordion.
    
    DFK-483

--- frontend/src/inngaaende-test/testregel-form/types.ts
+++ frontend/src/inngaaende-test/testregel-form/types.ts
@@ -2,6 +2,7 @@ import { Svar } from '@test/api/types';
 import { TestregelSkjema } from '@test/util/testregelParser';
 
 export type SkjemaMedSvar = {
+  resultatId: number;
   skjema: TestregelSkjema;
   svar: Svar[];
 };
