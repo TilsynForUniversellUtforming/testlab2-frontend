ba596f5ab8160a018f5aa2e1172889ebad75ca84
Lars Lønne <lonnelars@gmail.com>
2024-03-12 10:44:47 +0100

    oppretter ny kontroll
    Lager et nytt endepunkt på server for å opprette kontroller, og kobler det nye skjemaet for opprett
    kontroll til dette endepunktet.
    
    DFK-500

--- frontend/src/kontroll/KontrollRoutes.tsx
+++ frontend/src/kontroll/KontrollRoutes.tsx
@@ -1,7 +1,9 @@
 import { AppRoute } from '@common/util/routeUtils';
+import { Heading } from '@digdir/design-system-react';
+import { Outlet } from 'react-router';
 import { RouteObject } from 'react-router-dom';
 
-import Kontroll from './Kontroll';
+import Kontroll, { action } from './Kontroll';
 
 export const KONTROLL_ROUTE: AppRoute = {
   navn: 'Kontroll',
@@ -10,7 +12,18 @@ export const KONTROLL_ROUTE: AppRoute = {
 
 export const KontrollRoutes: RouteObject = {
   path: 'kontroll',
-  element: <Kontroll />,
-  handle: { name: 'Kontroll' },
-  children: [],
+  element: <Outlet />,
+  children: [
+    {
+      path: 'opprett-kontroll',
+      element: <Kontroll />,
+      handle: { name: 'Kontroll' },
+      action: action,
+    },
+    {
+      path: ':kontroll-id/velg-losninger',
+      element: <Heading level={1}>Velg løsninger</Heading>,
+      handle: { name: 'Velg løsninger' },
+    },
+  ],
 };
---
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/kontroll/KontrollResource.kt
@@ -0,0 +1,44 @@
+package no.uutilsynet.testlab2frontendserver.kontroll
+
+import no.uutilsynet.testlab2frontendserver.common.TestingApiProperties
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+
+@RestController
+@RequestMapping("api/v1/kontroller")
+class KontrollResource(
+    val restTemplate: RestTemplate,
+    val testingApiProperties: TestingApiProperties
+) {
+  private val logger: Logger = LoggerFactory.getLogger(KontrollResource::class.java)
+
+  @PostMapping
+  fun createKontroll(@RequestBody opprettKontroll: OpprettKontroll) =
+      runCatching {
+            val location =
+                restTemplate.postForLocation(
+                    testingApiProperties.url + "/kontroller", opprettKontroll)
+            val kontrollId =
+                location?.path?.substringAfterLast("/")?.toInt()
+                    ?: throw RuntimeException(
+                        "En ny kontroll ble opprettet, men vi fikk ikke noen location fra serveren.")
+            mapOf("kontrollId" to kontrollId)
+          }
+          .getOrElse {
+            logger.error("Oppretting av kontroll feilet", it)
+            throw RuntimeException(it)
+          }
+
+  data class OpprettKontroll(
+      val kontrolltype: String,
+      val tittel: String,
+      val saksbehandler: String,
+      val sakstype: String,
+      val arkivreferanse: String,
+  )
+}
