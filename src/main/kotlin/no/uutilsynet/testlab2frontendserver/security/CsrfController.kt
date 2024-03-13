d9ecaa0efbf13c728ef0463be5d2ab3800790e5d
oho <oystein.hoyum@digdir.no>
2024-03-13 10:11:33 +0100

    DFK-508

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/security/CsrfController.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/security/CsrfController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class CsrfController {
-  @RequestMapping("/csrf")
+  @RequestMapping("api/csrf")
   fun csrf(token: CsrfToken): CsrfToken {
     return token
   }
