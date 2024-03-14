aced40420367279348de09fbe4d3bcebe7571add
Lars Lønne <lonnelars@gmail.com>
2024-03-14 10:41:01 +0100

    fix: sletter kontroll etter test
    Playwright-testen for opprett kontroll lager en ny kontroll. Her sletter vi den igjen, så ikke databasen
    fylles opp av testkontroller.

--- frontend/e2e-tests/kontroll.spec.ts
+++ frontend/e2e-tests/kontroll.spec.ts
@@ -1,4 +1,15 @@
-import { expect, test } from '@playwright/test';
+import { expect, Page, request, test } from '@playwright/test';
+
+const deleteThese: number[] = [];
+
+function saveKontrollId(page: Page) {
+  const url = page.url();
+  const pattern = /\/kontroll\/(\d+)/;
+  const match = url.match(pattern);
+  if (match) {
+    deleteThese.push(parseInt(match[1]));
+  }
+}
 
 test('send inn skjema', async ({ page }) => {
   await page.goto('/kontroll/opprett-kontroll');
@@ -13,4 +24,13 @@ test('send inn skjema', async ({ page }) => {
   await expect(
     page.getByRole('heading', { name: 'Velg løsninger' })
   ).toBeVisible();
+
+  saveKontrollId(page);
+});
+
+test.afterAll(async () => {
+  for (const id of deleteThese) {
+    const requestContext = await request.newContext();
+    await requestContext.delete('/api/v1/kontroller/' + id);
+  }
 });
