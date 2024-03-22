cf8cdb2e032d1436ae8b70a5217de6dd2ffb83d9
Tore Fotland <tore.jacobsen.fotland@digdir.no>
2024-03-22 15:16:03 +0100

    DFK-523 - [BUGFIX] Feil prop drilling for oppdater resultat funksjon førte til at testResultsLoeysing state ikke ble oppdatert riktig - Lagt inn try catch på sletting av testelementer

--- frontend/src/inngaaende-test/types.ts
+++ frontend/src/inngaaende-test/types.ts
@@ -1,7 +1,8 @@
 import { Sak } from '@sak/api/types';
+import { TestregelResultat } from '@test/util/testregelParser';
 import { InnhaldstypeTesting, Testregel } from '@testreglar/api/types';
 
-import { ResultatManuellKontroll } from './api/types';
+import { ResultatManuellKontroll, Svar } from './api/types';
 
 export type ManuellTestStatus =
   | 'ferdig'
@@ -33,4 +34,12 @@ export type PageType = {
 export type ActiveTest = {
   testregel: Testregel;
   testResultList: ResultatManuellKontroll[];
+};
+
+export type TestResultUpdate = {
+  resultatId: number;
+  alleSvar: Svar[];
+  resultat?: TestregelResultat;
+  elementOmtale?: string;
+  kommentar?: string;
 };
