519f4f856e795cfb5986613267eb33fa79d493f0
Lars Lønne <lonnelars@gmail.com>
2024-05-07 11:15:02 +0200

    fix: spotless

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/kontroll/KontrollResource.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/kontroll/KontrollResource.kt
@@ -14,95 +14,93 @@ class KontrollResource(
     val restTemplate: RestTemplate,
     val testingApiProperties: TestingApiProperties,
 ) {
-    private val logger: Logger = LoggerFactory.getLogger(KontrollResource::class.java)
+  private val logger: Logger = LoggerFactory.getLogger(KontrollResource::class.java)
 
-    data class KontrollListItem(
-        val id: Int,
-        val tittel: String,
-        val saksbehandler: String,
-        val sakstype: String,
-        val arkivreferanse: String
-    )
+  data class KontrollListItem(
+      val id: Int,
+      val tittel: String,
+      val saksbehandler: String,
+      val sakstype: String,
+      val arkivreferanse: String
+  )
 
-    @GetMapping
-    fun getKontroller(): List<KontrollListItem> {
-        return runCatching {
-            restTemplate.getList<KontrollListItem>(testingApiProperties.url + "/kontroller")
-        }.getOrElse {
-            logger.error("Henting av alle kontroller feilet", it)
-            throw RuntimeException(it)
+  @GetMapping
+  fun getKontroller(): List<KontrollListItem> {
+    return runCatching {
+          restTemplate.getList<KontrollListItem>(testingApiProperties.url + "/kontroller")
         }
-    }
+        .getOrElse {
+          logger.error("Henting av alle kontroller feilet", it)
+          throw RuntimeException(it)
+        }
+  }
 
-    @PostMapping
-    fun createKontroll(@RequestBody opprettKontroll: OpprettKontroll) =
-        runCatching {
+  @PostMapping
+  fun createKontroll(@RequestBody opprettKontroll: OpprettKontroll) =
+      runCatching {
             val location =
                 restTemplate.postForLocation(
-                    testingApiProperties.url + "/kontroller", opprettKontroll
-                )
+                    testingApiProperties.url + "/kontroller", opprettKontroll)
             val kontrollId =
                 location?.path?.substringAfterLast("/")?.toInt()
                     ?: throw RuntimeException(
-                        "En ny kontroll ble opprettet, men vi fikk ikke noen location fra serveren."
-                    )
+                        "En ny kontroll ble opprettet, men vi fikk ikke noen location fra serveren.")
             mapOf("kontrollId" to kontrollId)
-        }
-            .getOrElse {
-                logger.error("Oppretting av kontroll feilet", it)
-                throw RuntimeException(it)
-            }
+          }
+          .getOrElse {
+            logger.error("Oppretting av kontroll feilet", it)
+            throw RuntimeException(it)
+          }
 
-    @GetMapping("{id}")
-    fun getKontroll(@PathVariable id: Int): ResponseEntity<*> {
-        val responseEntity =
-            restTemplate.getForEntity(testingApiProperties.url + "/kontroller/$id", String::class.java)
-        return ResponseEntity.status(responseEntity.statusCode).body(responseEntity.body)
-    }
+  @GetMapping("{id}")
+  fun getKontroll(@PathVariable id: Int): ResponseEntity<*> {
+    val responseEntity =
+        restTemplate.getForEntity(testingApiProperties.url + "/kontroller/$id", String::class.java)
+    return ResponseEntity.status(responseEntity.statusCode).body(responseEntity.body)
+  }
 
-    @DeleteMapping("{id}")
-    fun deleteKontroll(@PathVariable id: Int) =
-        runCatching { restTemplate.delete(testingApiProperties.url + "/kontroller/$id") }
-            .getOrElse {
-                logger.error("Sletting av kontroll feilet", it)
-                throw RuntimeException(it)
-            }
+  @DeleteMapping("{id}")
+  fun deleteKontroll(@PathVariable id: Int) =
+      runCatching { restTemplate.delete(testingApiProperties.url + "/kontroller/$id") }
+          .getOrElse {
+            logger.error("Sletting av kontroll feilet", it)
+            throw RuntimeException(it)
+          }
 
-    @PutMapping("{id}")
-    fun updateKontroll(
-        @PathVariable id: Int,
-        @RequestBody kontrollUpdate: KontrollUpdate
-    ): ResponseEntity<Unit> =
-        runCatching {
+  @PutMapping("{id}")
+  fun updateKontroll(
+      @PathVariable id: Int,
+      @RequestBody kontrollUpdate: KontrollUpdate
+  ): ResponseEntity<Unit> =
+      runCatching {
             restTemplate.put(
-                testingApiProperties.url + "/kontroller/{id}", kontrollUpdate, mapOf("id" to id)
-            )
+                testingApiProperties.url + "/kontroller/{id}", kontrollUpdate, mapOf("id" to id))
             ResponseEntity.noContent().build<Unit>()
-        }
-            .getOrElse {
-                logger.error("Oppdatering av kontroll feilet")
-                ResponseEntity.internalServerError().build()
-            }
+          }
+          .getOrElse {
+            logger.error("Oppdatering av kontroll feilet")
+            ResponseEntity.internalServerError().build()
+          }
 
-    @GetMapping("sideutvaltype")
-    fun getSideutvalType(): List<SideutvalType> =
-        try {
-            restTemplate.getList<SideutvalType>("${testingApiProperties.url}/kontroller/sideutvaltype")
-        } catch (e: Error) {
-            logger.error("klarte ikke å hente sideutvaltyper", e)
-            throw Error("Klarte ikke å hente sideutvaltyper")
-        }
+  @GetMapping("sideutvaltype")
+  fun getSideutvalType(): List<SideutvalType> =
+      try {
+        restTemplate.getList<SideutvalType>("${testingApiProperties.url}/kontroller/sideutvaltype")
+      } catch (e: Error) {
+        logger.error("klarte ikke å hente sideutvaltyper", e)
+        throw Error("Klarte ikke å hente sideutvaltyper")
+      }
 
-    data class SideutvalType(
-        val id: Int,
-        val type: String,
-    )
+  data class SideutvalType(
+      val id: Int,
+      val type: String,
+  )
 
-    data class OpprettKontroll(
-        val kontrolltype: String,
-        val tittel: String,
-        val saksbehandler: String,
-        val sakstype: String,
-        val arkivreferanse: String,
-    )
+  data class OpprettKontroll(
+      val kontrolltype: String,
+      val tittel: String,
+      val saksbehandler: String,
+      val sakstype: String,
+      val arkivreferanse: String,
+  )
 }
