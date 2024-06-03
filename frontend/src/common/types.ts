5b839b6ea1cf0921bb761d3f6e0b0db44346f585
torefotland <121802694+torefotland@users.noreply.github.com>
2024-06-03 12:14:33 +0200

    DFK-569 (#132)
    * DFK-569
    - Justeringer ihht. Figma i sideutval steg
    - [WIP] Tydeliggjøre ta bort side i sideutval
    
    * DFK-569
    - Justeringer ihht. Figma i sideutval steg
    - Tydeliggjøre ta bort side i sideutval
    
    * DFK-569
    - Korrigert indeksering av cards
    
    * DFK-569
    - Byttet variant på knapp for fjerning av side

--- frontend/src/common/types.ts
+++ frontend/src/common/types.ts
@@ -46,17 +46,17 @@ export type ButtonVariantType =
   (typeof ButtonVariant)[keyof typeof ButtonVariant];
 
 export const ButtonSize = {
-  Small: 'small',
-  Medium: 'medium',
-  Large: 'large',
+  Small: 'sm',
+  Medium: 'md',
+  Large: 'lg',
 } as const;
 
 export type ButtonSizeType = (typeof ButtonSize)[keyof typeof ButtonSize];
 
 export const CheckboxSize = {
-  Small: 'small',
-  Medium: 'medium',
-  Large: 'large',
+  Small: 'sm',
+  Medium: 'md',
+  Large: 'lg',
 } as const;
 
 export type CheckboxSizeType = (typeof CheckboxSize)[keyof typeof CheckboxSize];
