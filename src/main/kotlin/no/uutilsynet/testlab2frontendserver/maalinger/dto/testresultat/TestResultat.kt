9d8e678a624f9c1a7b19f9e138de337def4c0e5a
oho <oystein.hoyum@digdir.no>
2024-06-05 15:03:50 +0200

    DFK-561 smileys og bildevisning

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/maalinger/dto/testresultat/TestResultat.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/maalinger/dto/testresultat/TestResultat.kt
@@ -2,6 +2,7 @@ package no.uutilsynet.testlab2frontendserver.maalinger.dto.testresultat
 
 import java.net.URL
 import java.time.LocalDateTime
+import no.uutilsynet.testlab2frontendserver.testing.Bilde
 
 data class TestResultat(
     val suksesskriterium: List<String>,
@@ -9,9 +10,10 @@ data class TestResultat(
     val testregelId: Int,
     val testregelNoekkel: String,
     val sideNivaa: Int,
-    val testVartUtfoert: LocalDateTime,
+    val testVartUtfoert: LocalDateTime?,
     val elementUtfall: String,
     val elementResultat: String,
     val elementOmtale: ElementOmtale? = null,
-    val kommentar: String?
+    val kommentar: String?,
+    val bilde: List<Bilde>?
 )
