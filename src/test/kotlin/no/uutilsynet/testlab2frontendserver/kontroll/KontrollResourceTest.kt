d93c50f4ddb50280581b7e34d2151a3bc8938ce2
Øystein Høyum <oystein.hoyum@digdir.no>
2025-01-06 14:39:39 +0100

    Bump frontend deps (#183)
    * dependeny oppdatering
    * Packageoppdatering es-lint

--- src/test/kotlin/no/uutilsynet/testlab2frontendserver/kontroll/KontrollResourceTest.kt
+++ src/test/kotlin/no/uutilsynet/testlab2frontendserver/kontroll/KontrollResourceTest.kt
@@ -5,10 +5,7 @@ import java.time.Instant
 import kotlin.properties.Delegates
 import no.uutilsynet.testlab2frontendserver.common.Brukar
 import no.uutilsynet.testlab2frontendserver.common.TestingApiProperties
-import no.uutilsynet.testlab2frontendserver.fakes.FakeTestgrunnlagAPIClient
-import no.uutilsynet.testlab2frontendserver.fakes.FakeTestresultatAPIClient
-import no.uutilsynet.testlab2frontendserver.fakes.fakeId
-import no.uutilsynet.testlab2frontendserver.fakes.faker
+import no.uutilsynet.testlab2frontendserver.fakes.*
 import no.uutilsynet.testlab2frontendserver.resultat.TestgrunnlagType
 import no.uutilsynet.testlab2frontendserver.testing.CreateTestResultat
 import no.uutilsynet.testlab2frontendserver.testing.ElementResultat
@@ -20,6 +17,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
 
 class KontrollResourceTest {
@@ -29,7 +27,7 @@ class KontrollResourceTest {
 
   @BeforeEach
   fun setUp() {
-    // Mocker disse avhengighetene, siden det ikke finnes noen fakes for dem. De brukes ikke i
+    // Mocker disse avhengighetene, siden det ikke finnes noen kers for dem. De brukes ikke i
     // testen. Hvis de skal brukes senere, er det bedre om vi lager fakes for dem.
     val mockRestTemplate = Mockito.mock(RestTemplate::class.java)
     val mockTestingApiProperties = Mockito.mock(TestingApiProperties::class.java)
@@ -50,7 +48,7 @@ class KontrollResourceTest {
     val nyttTestgrunnlag =
         TestgrunnlagAPIClient.NyttTestgrunnlag(
             kontrollId = kontrollId,
-            namn = faker.lorem().sentence(),
+            namn = testgrunnlagNamne,
             type = TestgrunnlagType.RETEST,
             sideutval = emptyList(),
             testregelIdList = emptyList(),
@@ -66,20 +64,16 @@ class KontrollResourceTest {
     val loeysingId = fakeId()
     val testregelId = fakeId()
     val sideutvalId = fakeId()
+
     val nyttTestgrunnlag =
         TestgrunnlagAPIClient.NyttTestgrunnlag(
             kontrollId = kontrollId,
-            namn = faker.lorem().sentence(),
+            namn = testgrunnlagNamne,
             type = TestgrunnlagType.RETEST,
             sideutval =
                 listOf(
                     Sideutval(
-                        loeysingId,
-                        fakeId(),
-                        faker.lorem().sentence(),
-                        URI(faker.internet().url()),
-                        null,
-                        sideutvalId)),
+                        loeysingId, fakeId(), siteutvalNamn, URI(dummyUrl), null, sideutvalId)),
             testregelIdList = listOf(testregelId),
         )
     val testgrunnlag = kontrollResource.nyttTestgrunnlag(kontrollId, nyttTestgrunnlag).body
@@ -91,20 +85,21 @@ class KontrollResourceTest {
             loeysingId = loeysingId,
             testregelId = testregelId,
             sideutvalId = sideutvalId,
-            brukar = Brukar(faker.internet().emailAddress(), faker.name().fullName()),
+            brukar = Brukar(emailAddress, brukarNamn),
             elementOmtale = null,
             elementResultat = null,
             elementUtfall = null,
             testVartUtfoert = null,
             kommentar = null)
     val testResult = testResource.createTestResultat(createTestResultat)
+
     testResource.updateResultatManuellKontroll(
         listOf(
             testResult.copy(
                 kommentar = "Ferdig",
-                elementOmtale = faker.lorem().sentence(),
+                elementOmtale = elementOmtale,
                 elementResultat = ElementResultat.brot,
-                elementUtfall = faker.lorem().word(),
+                elementUtfall = elementUtfall,
                 testVartUtfoert = Instant.now(),
                 status = ResultatManuellKontroll.Status.Ferdig)))
 
@@ -120,17 +115,12 @@ class KontrollResourceTest {
     val nyttTestgrunnlag =
         TestgrunnlagAPIClient.NyttTestgrunnlag(
             kontrollId = kontrollId,
-            namn = faker.lorem().sentence(),
+            namn = testgrunnlagNamne,
             type = TestgrunnlagType.RETEST,
             sideutval =
                 listOf(
                     Sideutval(
-                        loeysingId,
-                        fakeId(),
-                        faker.lorem().sentence(),
-                        URI(faker.internet().url()),
-                        null,
-                        sideutvalId)),
+                        loeysingId, fakeId(), "Eit sideutval", URI(dummyUrl), null, sideutvalId)),
             testregelIdList = listOf(testregelId),
         )
     val testgrunnlag = kontrollResource.nyttTestgrunnlag(kontrollId, nyttTestgrunnlag).body
@@ -142,7 +132,7 @@ class KontrollResourceTest {
             loeysingId = loeysingId,
             testregelId = testregelId,
             sideutvalId = sideutvalId,
-            brukar = Brukar(faker.internet().emailAddress(), faker.name().fullName()),
+            brukar = Brukar(emailAddress, brukarNamn),
             elementOmtale = null,
             elementResultat = null,
             elementUtfall = null,
@@ -155,13 +145,13 @@ class KontrollResourceTest {
         listOf(
             testResult.copy(
                 kommentar = "Ferdig",
-                elementOmtale = faker.lorem().sentence(),
+                elementOmtale = elementOmtale,
                 elementResultat = ElementResultat.brot,
-                elementUtfall = faker.lorem().word(),
+                elementUtfall = elementUtfall,
                 testVartUtfoert = Instant.now(),
                 status = ResultatManuellKontroll.Status.Ferdig)))
 
-    val retestResponse =
+    val retestResponse: ResponseEntity<KontrollResource.TestgrunnlagDTO> =
         kontrollResource.createRetest(kontrollId, Retest(testgrunnlagId, loeysingId))
 
     assertThat(retestResponse.statusCode).isEqualTo(HttpStatus.OK)
@@ -175,17 +165,12 @@ class KontrollResourceTest {
     val nyttTestgrunnlag =
         TestgrunnlagAPIClient.NyttTestgrunnlag(
             kontrollId = kontrollId,
-            namn = faker.lorem().sentence(),
+            namn = "Eit testgrunnlag",
             type = TestgrunnlagType.RETEST,
             sideutval =
                 listOf(
                     Sideutval(
-                        loeysingId,
-                        fakeId(),
-                        faker.lorem().sentence(),
-                        URI(faker.internet().url()),
-                        null,
-                        sideutvalId)),
+                        loeysingId, fakeId(), "Eit sideutval", URI(dummyUrl), null, sideutvalId)),
             testregelIdList = listOf(testregelId),
         )
     val testgrunnlag = kontrollResource.nyttTestgrunnlag(kontrollId, nyttTestgrunnlag).body
@@ -197,7 +182,7 @@ class KontrollResourceTest {
             loeysingId = loeysingId,
             testregelId = testregelId,
             sideutvalId = sideutvalId,
-            brukar = Brukar(faker.internet().emailAddress(), faker.name().fullName()),
+            brukar = Brukar(emailAddress, brukarNamn),
             elementOmtale = null,
             elementResultat = null,
             elementUtfall = null,
