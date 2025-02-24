665e1380b11805ebfb3962b2128101f522d9018a
Øystein Høyum <oystein.hoyum@digdir.no>
2025-02-24 10:54:55 +0100

    Dfk 693 bildehandtering (#199)
    * DFK-693

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/resultat/BildeResource.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/resultat/BildeResource.kt
@@ -2,24 +2,31 @@ package no.uutilsynet.testlab2frontendserver.resultat
 
 import java.net.HttpURLConnection
 import java.net.Proxy
-import java.net.URI
 import no.uutilsynet.testlab2frontendserver.common.TestingApiProperties
 import org.springframework.core.io.InputStreamResource
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
 @RequestMapping("api/bilder")
 class BildeResource(val testingApiProperties: TestingApiProperties) {
 
-  @GetMapping("/{bilde}")
-  fun getBilde(@PathVariable bilde: String): ResponseEntity<InputStreamResource> {
-    val bildeUrl = URI("${testingApiProperties.url}/bilder/sti/$bilde").toURL()
+  @GetMapping()
+  fun getBilde(@RequestParam bildesti: String): ResponseEntity<InputStreamResource> {
+    val bildeUrl =
+        UriComponentsBuilder.fromUriString("${testingApiProperties.url}/bilder/sti")
+            .queryParam("bildesti", bildesti)
+            .build()
+            .toUri()
+            .toURL()
+
+    val filename = bildesti.split("/").last()
 
     val connection = bildeUrl.openConnection(Proxy.NO_PROXY) as HttpURLConnection
     connection.setRequestProperty(testingApiProperties.headerName, testingApiProperties.key)
@@ -27,7 +34,7 @@ class BildeResource(val testingApiProperties: TestingApiProperties) {
     val contentType: String = connection.contentType ?: "image/jpeg"
     val mediaType = MediaType.parseMediaType(contentType)
     val headers = HttpHeaders()
-    headers["Content-Disposition"] = "inline; filename=\"$bilde\""
+    headers["Content-Disposition"] = "inline; filename=\"$filename\""
     headers[testingApiProperties.headerName] = testingApiProperties.key
 
     return ResponseEntity.ok()
--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/resultat/ResultatResource.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/resultat/ResultatResource.kt
@@ -141,6 +141,7 @@ class ResultatResource(
   }
 
   private fun mapBilder(testresultat: TestResultat): TestResultat {
+    logger.info("Bilder: ${testresultat.bilder}")
     val nyeBilder = testresultat.bilder?.map { bildeService.proxyUrl(it) }
     return testresultat.copy(bilder = nyeBilder)
   }
