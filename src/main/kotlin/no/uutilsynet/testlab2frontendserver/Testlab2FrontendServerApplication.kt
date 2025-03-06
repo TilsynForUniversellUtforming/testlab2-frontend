798bf488af4e23f8f9bb3d1b39f00220fb4b1f5b
Øystein Høyum <oystein.hoyum@digdir.no>
2025-03-06 11:37:01 +0100

    DFK-713 (#204)
    * DFK-713

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/Testlab2FrontendServerApplication.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/Testlab2FrontendServerApplication.kt
@@ -1,6 +1,5 @@
 package no.uutilsynet.testlab2frontendserver
 
-import no.uutilsynet.testlab2securitylib.interceptor.BearerTokenInterceptor
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
@@ -8,7 +7,6 @@ import org.springframework.boot.runApplication
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Profile
-import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication(
@@ -20,11 +18,9 @@ class Testlab2FrontendServerApplication(val restTemplateBuilder: RestTemplateBui
 
   @Bean
   @Profile("security")
-  fun restTemplateSecurity(bearerTokenInterceptor: BearerTokenInterceptor): RestTemplate {
-    val interceptors: ArrayList<ClientHttpRequestInterceptor> = ArrayList()
-    interceptors.add(bearerTokenInterceptor)
+  fun restTemplateSecurity(): RestTemplate {
 
-    return restTemplateBuilder.interceptors(interceptors).build()
+    return restTemplateBuilder.build()
   }
 
   @Bean
