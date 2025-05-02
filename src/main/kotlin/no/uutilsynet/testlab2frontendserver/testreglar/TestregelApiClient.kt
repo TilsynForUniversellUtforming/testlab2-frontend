f4099c40dce6cebe27e6e804d2cbd759a88336ae
oho <oystein.hoyum@digdir.no>
2025-05-02 11:37:04 +0200

    DFK-765

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/testreglar/TestregelApiClient.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/testreglar/TestregelApiClient.kt
@@ -10,18 +10,21 @@ import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
 @Component
-class TestregelApiClient(val restTemplate: RestTemplate,  testingApiProperties: TestingApiProperties) {
+class TestregelApiClient(
+    val restTemplate: RestTemplate,
+    testingApiProperties: TestingApiProperties
+) {
 
-    val testregelUrl = "${testingApiProperties.url}/v1/testreglar"
+  val testregelUrl = "${testingApiProperties.url}/v1/testreglar"
 
-    fun getInnhaldstypeForTestingList() =
-        restTemplate.getList<InnhaldstypeTesting>("$testregelUrl/innhaldstypeForTesting")
+  fun getInnhaldstypeForTestingList() =
+      restTemplate.getList<InnhaldstypeTesting>("$testregelUrl/innhaldstypeForTesting")
 
-    fun getTemaForTestreglar() = restTemplate.getList<Tema>("$testregelUrl/temaForTestreglar")
+  fun getTemaForTestreglar() = restTemplate.getList<Tema>("$testregelUrl/temaForTestreglar")
 
-    fun getTestobjektForTesting() = restTemplate.getList<Testobjekt>("$testregelUrl/testobjektForTestreglar")
+  fun getTestobjektForTesting() =
+      restTemplate.getList<Testobjekt>("$testregelUrl/testobjektForTestreglar")
 
-    fun getTestregelListWithMetadata() = restTemplate.getList<TestregelDTO>("$testregelUrl?includeMetadata=true")
-
-
+  fun getTestregelListWithMetadata() =
+      restTemplate.getList<TestregelDTO>("$testregelUrl?includeMetadata=true")
 }
