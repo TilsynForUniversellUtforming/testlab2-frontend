ed0ba1af6bc32dcfd5e9e2479bca1fc717b25cf5
oho <oystein.hoyum@digdir.no>
2025-09-01 12:51:48 +0200

    DFK-765 Flytt aggregering til backend

--- frontend/src/inngaaende-test/test-overview/loeysing-test/TestOverviewLoeysing.tsx
+++ frontend/src/inngaaende-test/test-overview/loeysing-test/TestOverviewLoeysing.tsx
@@ -61,6 +61,9 @@ const TestOverviewLoeysing = () => {
     testKeys,
   } = useLoaderData() as TestOverviewLoaderResponse;
 
+  console.log('Testreglar for løysing:', testreglarForLoeysing);
+  console.log('Innhaldstype ' + JSON.stringify(innhaldstypeList));
+
   const [testResults, setTestResults] = useState<ResultatManuellKontroll[]>(
     testResultatForLoeysing
   );
