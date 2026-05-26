ee7eed05a6821e4d47a18f6e4ac30ec1b6f2500f
hoyum <oystein.hoyum@digdir.no>
2026-05-26 11:45:03 +0200

    DFK-744

--- src/main/kotlin/no/uutilsynet/testlab2frontendserver/kontroll/Kontroll.kt
+++ src/main/kotlin/no/uutilsynet/testlab2frontendserver/kontroll/Kontroll.kt
@@ -1,17 +1,23 @@
 package no.uutilsynet.testlab2frontendserver.kontroll
 
+import no.uutilsynet.testlab2.constants.Kontrolltype
+import no.uutilsynet.testlab2.constants.Sakstype
 import no.uutilsynet.testlab2frontendserver.maalinger.dto.Loeysing
 import no.uutilsynet.testlab2frontendserver.testreglar.dto.InnhaldstypeTesting
+import no.uutilsynet.testlab2frontendserver.utval.UtvalResource
+import java.time.LocalDate
 
 data class Kontroll(
     val id: Int,
-    val kontrolltype: String,
+    val kontrolltype: Kontrolltype,
     val tittel: String,
     val saksbehandler: String,
-    val sakstype: String,
+    val sakstype: Sakstype,
     val arkivreferanse: String,
     val loeysingar: List<Loeysing> = emptyList(),
-    val sideutvalList: List<Sideutval> = emptyList()
+    val utval: UtvalResource.Utval? = null,
+    val sideutvalList: List<Sideutval> = emptyList(),
+    val opprettaDato: LocalDate = LocalDate.now(),
 )
 
 data class KontrollTestingMetadata(
