ad1d6fb711927e7d2570556d7d503b3d0f4961b0
hoyum <oystein.hoyum@digdir.no>
2026-09-14 15:40:07 +0200

    DFK-726 detaljvising på regelsett

--- frontend/src/kontroll/velg-testreglar/RegelsettSelector.tsx
+++ frontend/src/kontroll/velg-testreglar/RegelsettSelector.tsx
@@ -1,8 +1,10 @@
-import { Alert } from '@digdir/designsystemet-react';
+import { Alert, Button, Dialog, Heading, Paragraph } from '@digdir/designsystemet-react';
 import { Regelsett, TestregelModus } from '@testreglar/api/types';
 import classNames from 'classnames';
 
 import classes from '../kontroll.module.css';
+import { InformationIcon } from '@navikt/aksel-icons';
+import React from 'react';
 
 interface Props {
   regelsettList: Regelsett[];
@@ -35,19 +37,42 @@ const RegelsettSelector = ({
   }
 
   return (
-    <div className={classes.velgRegelsettButtons}>
+    <ul className={classes.velgRegelsettButtons}>
       {regelsettList.map((regelsett) => (
-        <button
-          onClick={() => onSelectRegelsett(regelsett.id)}
-          className={classNames({
-            [classes.selected]: regelsett.id === selectedRegelsettId,
-          })}
-          key={regelsett.id}
-        >
-          {regelsett.namn}
-        </button>
+        <li key={regelsett.id} className={classes.utvalgPreviewWrapper}>
+          <Button
+            onClick={() => onSelectRegelsett(regelsett.id)}
+            className={classNames(classes.utvalgValgButton, {
+              [classes.selected]: regelsett.id === selectedRegelsettId,
+            })}
+          >
+            {regelsett.namn}
+          </Button>
+          <Button
+            className={classes.utvalgPreviewTrigger}
+            command="show-modal"
+            commandfor={`utval-preview-modal-${regelsett.id}`}
+            data-utvalid={regelsett.id}
+            data-variant={'secondary'}
+            icon
+          >
+            <InformationIcon title="a11y-title" fontSize="1.5rem" />
+          </Button>
+          <Dialog id={`utval-preview-modal-${regelsett.id}`}>
+            <Heading style={{ marginBottom: 'var(--ds-size-2)' }}>
+              Testreglar i utval: {regelsett.namn}
+            </Heading>
+            <Paragraph style={{ marginBottom: 'var(--ds-size-2)' }}>
+              <ul>
+                {regelsett.testregelList.map((testregel) => {
+                  return <li key={testregel.id}>{testregel.namn}</li>;
+                })}
+              </ul>
+            </Paragraph>
+          </Dialog>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 };
 
